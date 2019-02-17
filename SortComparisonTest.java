7d25cfdc6215e1e34de492e701da3935460d4a29
Hannah Keating <34624322+RandamQueen@users.noreply.github.com>
2019-02-17 13:59:39 +0000

    Added other sort tests
    There is problems with merge that need to be resolved.

--- SortComparisonTest.java
+++ SortComparisonTest.java
@@ -44,6 +44,60 @@ public class SortComparisonTest
     		assertEquals("The last elements are not equal",true,correctArray[size-1] == testArray[size-1] );
     		assertArrayEquals(correctArray,  testArray); 
     }
+    
+      @Test 
+    public void testQuick() 
+    { 
+    		new SortComparison(); 
+    		double[] testArray = {3,4,10,17,1,2,5,12,9,0};
+    		double [] correctArray = {0,1,2,3,4,5,9,10,12,17}; 
+    		int size = testArray.length; 
+    		testArray = SortComparison.quickSort(testArray); 
+    		assertEquals("The first elements are not equal",true,correctArray[0] == testArray[0] );
+    		assertEquals("The last elements are not equal",true,correctArray[size-1] == testArray[size-1] );
+    		assertArrayEquals(correctArray,  testArray); 
+    }
+    
+    @Test 
+    public void testMergeIterative() 
+    { 
+    		new SortComparison(); 
+    		double[] testArray = {3,4,10,17,1,2,5,12,9,0};
+    		double [] correctArray = {0,1,2,3,4,5,9,10,12,17}; 
+    		int size = testArray.length; 
+    		testArray = SortComparison.mergeSortIterative(testArray); 
+    		assertEquals("The first elements are not equal",true,correctArray[0] == testArray[0] );
+    		assertEquals("The last elements are not equal",true,correctArray[size-1] == testArray[size-1] );
+    		assertArrayEquals(correctArray,  testArray); 
+    }
+    
+    @Test 
+    public void testMergeRecursive() 
+    { 
+    		new SortComparison(); 
+    		double[] testArray = {3,4,10,17,1,2,5,12,9,0};
+    		double [] correctArray = {0,1,2,3,4,5,9,10,12,17}; 
+    		int size = testArray.length; 
+    		testArray = SortComparison.mergeSortRecursive(testArray); 
+    		assertEquals("The first elements are not equal",true,correctArray[0] == testArray[0] );
+    		assertEquals("The last elements are not equal",true,correctArray[size-1] == testArray[size-1] );
+    		assertArrayEquals(correctArray,  testArray); 
+    }
+
+    
+    @Test 
+    public void testSelection() 
+    { 
+    		new SortComparison(); 
+    		double[] testArray = {3,4,10,17,1,2,5,12,9,0};
+    		double [] correctArray = {0,1,2,3,4,5,9,10,12,17}; 
+    		int size = testArray.length; 
+    		testArray = SortComparison.selectionSort(testArray); 
+    		assertEquals("The first elements are not equal",true,correctArray[0] == testArray[0] );
+    		assertEquals("The last elements are not equal",true,correctArray[size-1] == testArray[size-1] );
+    		assertArrayEquals(correctArray,  testArray); 
+    }
+    
     // ----------------------------------------------------------
     /**
      *  Main Method.
